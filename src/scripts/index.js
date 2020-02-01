62318d04ddaaeae019f9463afdfd6be84b64f825
徐承 <xucheng.pc@alibaba-inc.com>
2020-02-01 22:26:24 +0800

    modify

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,17 @@
 import framework from './framework';
 import { init, config } from '../sdk/js-runtime/index';
 
-config.framework = framework;
-init(config);
+global = global || globalThis;
 
-export default framework;
+config.framework = framework;
+const globalMethods = init(config);
+
+for (let methodName in globalMethods) {
+  global[methodName] = (...args) => {
+    const ret = globalMethods[methodName](...args);
+    if (ret instanceof Error) {
+      console.error(ret.toString());
+    }
+    return ret;
+  }
+}
--- src/sdk/js-runtime/init.js
+++ src/sdk/js-runtime/init.js
@@ -1,9 +1,30 @@
 import { init as initTaskHandler } from './task-center';
 
+const instanceMap = {};
+let framework;
+
+function createInstance(id, code) {
+  let info = instanceMap[id];
+  if (!info) {
+    instanceMap[id] = {
+      created: Date.now(),
+      framework: 'xRender',
+    }
+    
+    return framework.createInstance(id, code);
+  }
+}
+
+const methods = {
+  createInstance,
+};
+
 export default function init (config) {
   initTaskHandler();
 
-  const framework = config.framework || {};
+  framework = config.framework || {};
 
   framework.init(config);
+
+  return methods;
 }
